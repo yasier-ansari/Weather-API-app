dc461629eed21be55e7add71b729cda878c62773
HyRaXxItY <yasirmohammed122002@gmail.com>
2022-06-01 12:10:21 +0530

    last

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // api key : 82005d27a116c2880c8f0fcb866998a0
 
+
 const notificationElement = document.querySelector(".notification");
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
